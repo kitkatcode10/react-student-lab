75b37afb776b9fad69454e5f9c95a627f6d02467
Katherine Wong <katherinewongkw3@gmail.com>
2021-07-14 17:55:06 -0700

    create students component

---
+++ components/Students/Students.jsx
@@ -0,0 +1,8 @@
+//make student component
+//render text saying student component
+//implemenet loop (mapping) that will render student component for every object in that array
+//pass student var as prop to student comp
+//check react dev tools that we got the value
+//then implement #2
+
+//#3 do same thing
